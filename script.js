96d2b5f47913c2aa18b34a59d6ce2b7b6f22f476
Solomon Omojola <omojolasolomon@gmail.com>
2016-09-03 09:29:12 +0100

    Adding todo items by pressing enter; Ticking off todo items

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
 let $todoText = $("#inputText");
 let $todoList = $("#listDiv");
 
-$("#addItem").click(function() {
+function appendTodo(todoText, todoList) {
 	//take what is in the input
-	let itemMarkup = `<p><input type="checkbox" name="todoItem" value="" class="todoItem"> ${ $todoText.val() }</p>`;
-	$todoList.append(itemMarkup);
-	$todoText.val("");
-
+	let itemMarkup = `<p><input type="checkbox" name="todoItem" value="${ todoText.val() }" class="todoItem"> ${ todoText.val() }</p>`;
 	//add it to the to do list
+	todoList.append(itemMarkup);
+	//reset the input
+	todoText.val("");
+}
+
+
+// appending by "add" button
+$("#addItem").click(function() {
+	if ($.trim($todoText.val()).length > 0) {
+		appendTodo($todoText, $todoList);
+	}
+});
+
+// appending by "enter" key
+$todoText.keydown(function(event) {
+	if ($.trim($todoText.val()).length > 0) {
+	    if (event.which == 13) appendTodo($todoText, $todoList);
+	}
+});
+
+
+
+
+$("#listDiv").on("change", ".todoItem", function() {
+	if ($(this).parent().hasClass("done")) {
+		$(this).parent().removeClass()
+	} else {
+		$(this).parent().addClass("done");
+	}
 });

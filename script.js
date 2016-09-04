2262ed83bb44bc125d89b6754be028f137a0cbbd
Solomon Omojola <omojolasolomon@gmail.com>
2016-09-04 16:35:02 +0100

    Delete button

--- script.js
+++ script.js
@@ -12,12 +12,18 @@ function loadStoredList(todoList) {
 	if(localStorage.getItem('todos')) {
 		todoList.html(localStorage.getItem('todos'));
 	}
-	console.log("loaded!");
+	console.log("List loaded from local storage!");
 }
 
 function appendTodo(todoText, todoList) {
 	//take what is in the input
-	let itemMarkup = `<p><input type="checkbox" name="todoItem" value="${ todoText.val() }" class="todoItem"> ${ todoText.val() }</p>`;
+	let itemMarkup = `
+	<p>
+		<input type="checkbox" name="todoItem" value="${ todoText.val() }" class="todoItem"> 
+		<span class="todoItemText">${ todoText.val() }</span> &nbsp 
+		<button class="delButton">del</button>
+	</p>
+	`;
 	//add it to the to do list
 	todoList.append(itemMarkup);
 	//reset the input
@@ -27,6 +33,7 @@ function appendTodo(todoText, todoList) {
 
 $("document").ready(function() {
 	loadStoredList($todoList);
+	console.log("Page loaded successfully!")
 })
 
 
@@ -53,13 +60,13 @@ $todoText.keydown(function(event) {
 $("#listDiv").on("change", ".todoItem", function() {
 	console.log("Something happened here!");
 	console.log($(this)[0].checked);
-	if ($(this).parent().hasClass("done")) {
-		$(this).parent().removeClass("done");
+	if ($(this).next().hasClass("done")) {
+		$(this).next().removeClass("done");
 		//had to use pure js cos i couldnt find a way to add/remove "checked" to/from the html with jquery
 		//since im using local storage, its not just behaviour that matters, but the html too so that the checked state is "remembered"
 		$(this)[0].removeAttribute("checked"); 
 	} else {
-		$(this).parent().addClass("done");
+		$(this).next().addClass("done");
 		//had to use pure js cos i couldnt find a way to add/remove "checked" to/from the html with jquery
 		//since im using local storage, its not just behaviour that matters, but the html too so that the checked state is "remembered"
 		$(this)[0].setAttribute("checked", "checked");
@@ -67,3 +74,9 @@ $("#listDiv").on("change", ".todoItem", function() {
 	saveList($todoList);
 });
 
+// delete button
+$("#listDiv").on("click", ".delButton", function() {
+	$(this).parent().remove();
+	console.log("You've deleted an item permanently!");
+	saveList($todoList);
+});
